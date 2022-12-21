78d8fdb073436637d57bc46451d64d52c0ada4ba
bendary <mohamedyossif577@gmail.com>
2022-12-21 14:10:06 -0700

    adding product model

--- src/main/java/com/mbbmecommerce/mbbm/controllers/ProductController.java
+++ src/main/java/com/mbbmecommerce/mbbm/controllers/ProductController.java
@@ -1,9 +1,17 @@
 package com.mbbmecommerce.mbbm.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.xml.ws.http.HTTPBinding;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/products/")
 public class ProductController {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable String productId){
+        return new ResponseEntity<>("", HttpStatus.OK);
+    }
 }
---
+++ src/main/java/com/mbbmecommerce/mbbm/models/Product.java
@@ -0,0 +1,4 @@
+package com.mbbmecommerce.mbbm.models;
+
+public class Product {
+}
